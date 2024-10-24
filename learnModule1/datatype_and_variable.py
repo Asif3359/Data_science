fd6ef99eac80082b1a5fe928c6c01b19776aec7c
Asif Ahammed <asifahammed359@gmail.com>
2024-10-24 18:23:36 +0600

    start data science 1

---
+++ learnModule1/datatype_and_variable.py
@@ -0,0 +1,14 @@
+
+x_int = 10
+y_int = 20
+sum_int = x_int + y_int
+print(x_int , " + " ,y_int ," = ", sum_int)
+
+
+x_float = 10.5
+y_float = 20.5
+sum_float = x_float + y_float
+print(x_float, " + ",y_float, " = ", sum_float)
+
+x_string = "Hello to the world of data science"
+print(x_string)
