2ba9844ab8520dac83cc588f8f3766be37e345bb
Asif Ahammed <asifahammed359@gmail.com>
2024-10-24 19:40:09 +0600

    for loop

---
+++ learnModule1/list_matrices_dectonary.py
@@ -0,0 +1,45 @@
+from unittest.mock import numerics
+
+print("List : ")
+fruits = ["mango", "apple", "banana", "orange"]
+print(len(fruits))
+print(fruits[0])
+print(fruits[len(fruits)-1])
+fruits[1] = "jackfruit"
+print(fruits)
+print()
+
+print("Matrices :")
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+print(matrix)
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        print(f"{matrix[i][j]}", end=" ")
+    print()
+print()
+
+
+print("2D Pattern : ")
+x = 5
+for i in range(x):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+print()
+
+
+print("Dictionary : ")
+resident = {
+    "name":"Asif Ahammed",
+    "city":"Dhaka",
+    "numbers":[10,12,13,14]
+}
+print(resident)
+resident["numbers"][1]=15
+for key,value in resident.items():
+    print(f"{key} : {value}")
+print()
